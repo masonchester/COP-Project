d07d2c9eb89bdd84978478533f352cb9576784ce
MikeG86 <michaelgrunk@gmail.com>
2023-05-01 21:29:43 -0400

    final update

--- Parser.py
+++ Parser.py
@@ -1,17 +1,14 @@
 from Scanner import *
-
+#parser class
 class Parser():
-
+    #constructor for parser
     def __init__(self, input_string):
         self.input_string = input_string
         self.current_token = get_next_token(self.input_string, True)
-    
+    #factor method for grammer
     def factor(self):
         token = self.current_token
-
         # check if the token is an integer or an identifier
-       
-        
         if token.token == 'INT_LIT':
             self.current_token = get_next_token(self.input_string, True)
             return int(token.lexeme)
@@ -27,10 +24,9 @@ class Parser():
             return results
         else:
             raise Exception('Invalid factor')
-
+    #term method for grammar
     def term(self):
         results = self.factor()
-
         while self.current_token.token in ['22', '23']:
             if self.current_token.token == '22':
                 self.current_token = get_next_token(self.input_string, True)
@@ -38,9 +34,8 @@ class Parser():
             elif self.current_token.token == '23':
                 self.current_token = get_next_token(self.input_string, True)
                 results /= self.factor()
-
         return results
-    
+    #expr method for grammar
     def expr(self):
         results = self.term()
 
@@ -51,27 +46,20 @@ class Parser():
             elif self.current_token.token == '21':
                 self.current_token = get_next_token(self.input_string, True)
                 results -= self.term()
-
         return results
+    #assignment method for grammar
     def assignment(self):
         token = self.current_token
-
         if token.token != 'IDENT':
             raise Exception('Invalid assignment')
         variable = token.lexeme
-
         self.current_token = get_next_token(self.input_string, True)
-
         if self.current_token.token != '26':
-
             raise Exception('Missing equals sign in assignment')
-        
         self.current_token = get_next_token(self.input_string, True)
-
         value = self.expr()
-
         return(variable,value)
-    
+    #statement method for grammar
     def statement(self):
         token = self.current_token
         if token.token == 'IDENT':
@@ -79,7 +67,7 @@ class Parser():
         else:
             return self.expr()
 
-    # parse the entire input string
+    # parse method starts
     def parse(self):
         results = self.statement()
         the_variables.append(results)
@@ -92,11 +80,12 @@ class Parser():
         if self.current_token.lexeme != 'EOF':
             raise Exception('Invalid syntax')
         return results
+#run the parser 
 the_file = open('C:\ConceptsProject\COP-Project\Example.txt')
 the_text = the_file.read()
 p = Parser(the_text)
 the_variables = []
 result = p.parse()
-
+#print the results
 for x in the_variables:
     print(x)
